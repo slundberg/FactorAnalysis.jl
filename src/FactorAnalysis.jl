8432ee67bfe3863f43aab06729768e4dd19f58ae
Scott Lundberg <scottmlundberg@gmail.com>
2015-10-20 15:57:13 -0700

    First gradient implementation.

--- src/FactorAnalysis.jl
+++ src/FactorAnalysis.jl
@@ -1,5 +1,8 @@
 module FactorAnalysis
 
 # package code goes here
+include("utils.jl")
+include("gradient.jl")
+include("em.jl")
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,5 @@
 using FactorAnalysis
 using Base.Test
 
-# write your own tests here
-@test 1 == 1
+include("utils.jl")
+include("gradient.jl")
